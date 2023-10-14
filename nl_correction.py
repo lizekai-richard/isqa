cf1630653dd98b9c1c8d0f9dbd2bcf865b81e6d2
lizekai-richard <lzknus@gmail.com>
2023-10-14 17:01:27 +0800

    Update batched correction

--- nl_correction.py
+++ nl_correction.py
@@ -1,9 +1,10 @@
 import json
 import os
-os.environ["CUDA_VISIBLE_DEVICES"]="2,3"
 import re
 import string
+import random
 import torch
+import spacy
 from collections import Counter
 from tqdm import tqdm
 from torch.utils.data import Dataset
@@ -13,6 +14,8 @@ from transformers import LlamaTokenizer, LlamaForCausalLM
 from peft import PeftModel, prepare_model_for_int8_training
 import argparse
 from inference_helper import StreamPeftGenerationMixin
+
+nlp = spacy.load("en_core_web_sm")
 
 
 class SciMRCDataset(Dataset):
@@ -186,8 +189,11 @@ def feedback_step(args, tokenizer, feedback_model, summary, question, answer):
 
 
 def refine_step(args, tokenizer, base_model, text, feedback):
+    # prompt = """
+    #     Below is a scientific paper. Please summarize the paper based on the provided facts and non-facts.\n###Paper: {text}\n###Facts: {facts}\n###Non-Facts: {non_facts}\n###Summary:
+    # """
     prompt = """
-        Below is a scientific paper. Please summarize the paper based on the provided facts and non-facts.\n###Paper: {text}\n###Facts: {facts}\n###Non-Facts: {non_facts}\n###Summary:
+        Below is a scientific paper. Please write a summary based on facts and non-facts we provide.\n###Paper: {text}\n###Facts: {facts}\n###Non-Facts: {non_facts}\n###Summary:
     """
     facts = ""
     for i, fact in enumerate(feedback['facts']):
@@ -217,6 +223,94 @@ def refine_step(args, tokenizer, base_model, text, feedback):
     output = tokenizer.decode(output_ids[0][len(input_ids[0]):], skip_special_tokens=True,
                               clean_up_tokenization_spaces=True)
     return output
+
+
+def create_mini_batch(indices, mini_batch_size):
+    return random.sample(indices, mini_batch_size)
+
+
+def similarity_filter(new_feedback, old_feedbacks):
+    p = nlp(new_feedback)
+    for of in old_feedbacks:
+        q = nlp(of)
+        similarity = p.similarity(q)
+        if similarity > 0.8:
+            return False
+    return True
+
+
+def batched_correction_stage(args, base_model, tokenizer, feedback_model, dataset):
+    results_to_save = {}
+    avg_f1_score = 0.0
+    tot_cnt = 0
+    with torch.no_grad():
+        for example in tqdm(dataset):
+            _id = example['id']
+            text = example['text'][:6000]
+            qa_pairs = example['qa_pairs']
+            gold_summary = example['summary']
+
+            results_to_save[_id] = []
+
+            initial_prompt = """
+                Please summarize the following scientific document.\n###Paper: {text}\n###Summary:
+            """.format(text=text)
+
+            input_ids = tokenizer(
+                initial_prompt,
+                max_length=args.max_length,
+                padding='max_length',
+                truncation=True,
+                return_tensors='pt'
+            ).input_ids.cuda()
+
+            output_ids = base_model.generate(
+                input_ids,
+                num_beams=args.num_beams,
+                max_new_tokens=args.gen_max_new_tokens,
+                min_new_tokens=args.gen_min_new_tokens,
+                temperature=args.temperature
+            )
+
+            pred_summary = tokenizer.decode(output_ids[0][len(input_ids[0]):], skip_special_tokens=True)
+            feedback = {'facts': [], 'non_facts': []}
+            indices = list(range(len(qa_pairs)))
+            best_score = 0
+            for i in range(args.num_correction_steps):
+                curr_batch = create_mini_batch(indices, args.correction_batch_size)
+                batch_scores = 0
+                batch_cnt = 0
+                for j in curr_batch:
+                    question, answer = qa_pairs[j][0], qa_pairs[j][1]
+                    feedback_dict = feedback_step(args, tokenizer, feedback_model, pred_summary, question, answer)
+                    if feedback_dict is None or feedback_dict['score'] == 0:
+                        continue
+                    # max_score_for_cur_example = max(max_score_for_cur_example, feedback_dict['score'])
+
+                    if "fact" in feedback_dict:
+                        if similarity_filter(feedback_dict['fact'], feedback['facts']):
+                            feedback['facts'].append(feedback_dict['fact'])
+                    elif "non_fact" in feedback_dict:
+                        if similarity_filter(feedback_dict['non_fact'], feedback['non_facts']):
+                            feedback['non_facts'].append(feedback_dict['non_fact'])
+                    batch_scores += feedback_dict['score']
+                    batch_cnt += 1
+                
+                if batch_cnt > 0:
+                    batch_avg_score = batch_scores / batch_cnt
+                    best_score = max(best_score, batch_avg_score)
+                    results_to_save[_id].append({
+                        'output': pred_summary, 
+                        'feedback': feedback,
+                        'f1-score': batch_avg_score
+                    })
+                    pred_summary = refine_step(args, tokenizer, base_model, text, feedback)
+
+            if best_score > 0:
+                avg_f1_score += best_score
+                tot_cnt += 1
+    print("Average F1 score after self-correction is: ", avg_f1_score/tot_cnt)
+    return results_to_save
 
 
 def correction_stage(args, base_model, tokenizer, feedback_model, dataset):
@@ -292,6 +386,7 @@ def correction_stage(args, base_model, tokenizer, feedback_model, dataset):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("--wandb", default=False)
+    parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--data_path", type=str, default="/path/to/data")
     parser.add_argument("--save_path", type=str, default="/path/to/save")
     parser.add_argument("--model_path", type=str, default="/path/to/model")
@@ -311,13 +406,19 @@ if __name__ == '__main__':
     parser.add_argument("--use_8bit", type=bool, default=True)
     parser.add_argument("--threshold", type=float, default=0.3)
     parser.add_argument("--patience", type=float, default=0.01)
+    parser.add_argument("--num_correction_steps", type=int, default=10)
+    parser.add_argument("--correction_batch_size", type=int, default=4)
     args = parser.parse_args()
+
+    torch.manual_seed(args.seed)
+    random.seed(args.seed)
 
     tokenizer, base_model = load_base_model(args)
     feedback_model = load_feedback_model(args)
 
     dataset = load_dataset("json", data_files=args.data_path)['train']
-    results_to_save = correction_stage(args, base_model, tokenizer, feedback_model, dataset.select(range(200, 300)))
+    # results_to_save = correction_stage(args, base_model, tokenizer, feedback_model, dataset.select(range(200, 300)))
+    results_to_save = batched_correction_stage(args, base_model, tokenizer, feedback_model, dataset.select(range(5)))
 
     with open(args.save_path, "w") as f:
         json.dump(results_to_save, f)
